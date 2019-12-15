c75d302cfb8270d8771695beac7b2ef44d0317b9
SpokyOky <a4ottruba@gmail.com>
2019-12-15 20:17:18 +0400

    comment fix

--- lab1WinForms/Program.cs
+++ lab1WinForms/Program.cs
@@ -8,15 +8,11 @@ namespace lab1WinForms
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormAircraftCarrier());
             Application.Run(new FormDocks());
         }
     }
