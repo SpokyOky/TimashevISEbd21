8b70579e51515fdb3a1051a880d5ddc7dbdf860c
SpokyOky <a4ottruba@gmail.com>
2019-12-15 21:02:23 +0400

    draw fix

--- lab1WinForms/Docks.cs
+++ lab1WinForms/Docks.cs
@@ -125,14 +125,6 @@ namespace lab1WinForms
                 i.Value.DrawTransport(g);
             }
 
-            for (int i = 0; i < maxPlaces; i++)
-            {
-                if (!CheckFreePlaces(i))
-                {
-                    places[i].DrawTransport(g);
-                }
-            }
-
             for (int i = 0; i < airplanes.Length; i++)
             {
                 if (airplanes[i] != null)
