0b4102ecf789ec3a740de06741314fb222b9d977
Evgen Timashev <timashev2001@mail.ru>
2019-12-16 13:22:55 +0400

    fix

---
+++ lab1WinForms/AircraftCarrier.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace lab1WinForms
+{
+    class AircraftCarrier : WarShip
+    {
+        public bool GunOnBoard { private set; get; }
+        public bool HelicoptersOnBoard { private set; get; }
+        public Airplanes.AirplanesCount Count { private set; get; }
+
+        public AircraftCarrier(int maxSpeed, double weight, Color primaryColor,
+            Color secondaryColor, Airplanes.AirplanesCount airplanesCount,
+            bool gunOnBoard = false, bool helicoptersOnBoard = false) : base (maxSpeed, weight, primaryColor, secondaryColor)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            PrimaryColor = primaryColor;
+            SecondaryColor = secondaryColor;
+            GunOnBoard = gunOnBoard; 
+            HelicoptersOnBoard = helicoptersOnBoard;
+            Count = airplanesCount;
+        }
+
+        public override void DrawTransport(Graphics g)
+        {
+            base.DrawTransport(g);
+            
+            //ВП полоса
+            Pen penSecondary = new Pen(SecondaryColor);
+            g.DrawLine(penSecondary, new Point(posX + 119, posY + 8), new Point(posX + 27, posY + 30));
+            g.DrawLine(penSecondary, new Point(posX + 119, posY + 20), new Point(posX + 40, posY + 38));
+            penSecondary.Dispose();
+
+            if (GunOnBoard)
+            {
+                Brush b = new SolidBrush(SecondaryColor);
+                //пушка
+                g.FillRectangle(b, posX + 65, posY + 5, 10, 8);
+
+                Brush red = new SolidBrush(Color.Red);
+                g.FillRectangle(red, posX + 60, posY + 6, 10, 2);
+                g.FillRectangle(red, posX + 60, posY + 10, 10, 2);
+                g.FillRectangle(red, posX + 67, posY + 6, 3, 6);
+
+                b.Dispose();
+                red.Dispose();
+            }
+
+            if (HelicoptersOnBoard)
+            {
+                //вертолёт
+                //тело
+                Brush b = new SolidBrush(SecondaryColor);
+                List<Point> pointsHelicopter = new List<Point>(3);
+                pointsHelicopter.Add(new Point(posX + 40, posY + 10));
+                pointsHelicopter.Add(new Point(posX + 44, posY + 10));
+                pointsHelicopter.Add(new Point(posX + 42, posY + 18));
+
+                g.FillPolygon(b, pointsHelicopter.ToArray<Point>());
+                b.Dispose();
+
+                //винты
+                Pen p = new Pen(Color.Black);
+                g.DrawLine(p, posX + 42, posY + 12, posX + 45, posY + 15);
+                g.DrawLine(p, posX + 42, posY + 12, posX + 39, posY + 15);
+                g.DrawLine(p, posX + 42, posY + 12, posX + 39, posY + 9);
+                g.DrawLine(p, posX + 42, posY + 12, posX + 45, posY + 9);
+                p.Dispose();
+            }
+
+            Airplanes airplanes = new Airplanes(Count, 
+                PrimaryColor, SecondaryColor, posX, posY);
+            airplanes.DrawAirplanes(g);
+        }
+    }
+}
